d17c085830539939f2968cd817bb3148b714ea55
bruno <brunomarino609@gmail.com>
2025-11-11 19:33:08 -0300

    migraciones, agregamos en admin Business y User

--- reservas/admin.py
+++ reservas/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
-from .models import Customer, Reservation, Resource
+from .models import Customer, Reservation, Resource, Business, User
 # Register your models here.
 
 admin.site.register(Customer)
 admin.site.register(Resource)
 admin.site.register(Reservation)
+admin.site.register(Business)
+admin.site.register(User)
